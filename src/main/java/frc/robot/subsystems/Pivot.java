5751d82df350ec988f1c120d31efb2952d939747
1062987 <adham.m.yeh@gmail.com>
2025-10-21 16:40:36 -0400

    ana msh ader 5alas ahhhhhhh

--- src/main/java/frc/robot/subsystems/Pivot.java
+++ src/main/java/frc/robot/subsystems/Pivot.java
@@ -1,6 +1,24 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkLowLevel;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.config.PivotConfig;
 
 public class Pivot extends SubsystemBase {
+  private SparkMax pivot = new SparkMax(1, SparkLowLevel.MotorType.kBrushless);
+  private PIDController pidController =
+      new PIDController(PivotConfig.PIVOT_P, PivotConfig.PIVOT_I, PivotConfig.PIVOT_D);
+
+  public Pivot() {
+    pivot.configure(
+        new SparkMaxConfig()
+            .smartCurrentLimit(PivotConfig.PIVOT_CURRENT_LIMIT)
+            .idleMode(PivotConfig.PIVOT_IDLE_MODE),
+        SparkBase.ResetMode.kNoResetSafeParameters,
+        SparkBase.PersistMode.kPersistParameters);
+  }
 }
