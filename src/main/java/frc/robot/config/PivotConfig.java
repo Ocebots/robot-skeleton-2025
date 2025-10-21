9d56fbb71cd3d6972e529806db3e59dac2b6cd5c
literal-gargoyle <mochithecorgi0117@gmail.com>
2025-10-21 16:39:44 -0400

    Pivot motor config DAVID IKEDA: USER: LITERAL-GARGOYLE

--- src/main/java/frc/robot/config/PivotConfig.java
+++ src/main/java/frc/robot/config/PivotConfig.java
@@ -1,4 +1,11 @@
 package frc.robot.config;
 
+import com.revrobotics.spark.config.SparkBaseConfig;
+
 public class PivotConfig {
+  public static final double PIVOT_P = 0.0;
+  public static final double PIVOT_I = 0.0;
+  public static final double PIVOT_D = 0.0;
+  public static final int PIVOT_CURRENT_LIMIT = 10;
+  public static final SparkBaseConfig.IdleMode PIVOT_IDLE_MODE = SparkBaseConfig.IdleMode.kBrake;
 }
